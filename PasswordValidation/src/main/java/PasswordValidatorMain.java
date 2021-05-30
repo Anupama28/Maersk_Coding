d4e25a96a2b37e1c0bb31ba8e016186d13870290
Anupama28 <33659513+Anupama28@users.noreply.github.com>
2021-05-30 19:48:35 +0530

    Add files via upload

---
+++ PasswordValidation/src/main/java/PasswordValidatorMain.java
@@ -0,0 +1,12 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class PasswordValidatorMain {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String password = sc.next();
+//        System.out.println("Password ::: " + password);
+
+        List<String> messages = PasswordValidator.isValidPassword("vhdfvdf1");
+    }
+}
